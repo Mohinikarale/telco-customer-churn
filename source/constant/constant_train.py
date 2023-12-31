a922d9db9c1735028e83b15ecf06a78848ac557a
Mohini Karale <karalemohini2002@gmail.com>
2023-12-31 11:31:36 +0530

    Project Architecture

---
+++ source/constant/constant_train.py
@@ -0,0 +1,18 @@
+#common constants
+
+TARGET_COLUMN = 'Churn'
+TRAIN_PIPELINE_NAME = 'train_pipeline'
+ARTIFACT_DIR = 'artifact'
+FILE_NAME = 'training_data.csv'
+
+TRAIN_FILE_NAME = 'train.csv'
+TEST_FILE_NAME = 'test.csv'
+
+MONGODB_URL_KEY = 'mongodb+srv://karaleguddi2:9579774398@mohinikarale.fgsd8ge.mongodb.net/?retryWrites=true&w=majority'
+DATABASE_NAME = 'db_customer_churn'
+
+DI_COLLECTION_NAME = 'telco-customer-churn'
+DI_DIR_NAME = 'data_ingetion'
+DI_FEATURE_STORE_DIR = 'feature_store'
+DI_INGESTED_DIR = 'ingested'
+DI_TRAIN_TEST_SPLIT_RATIO = 0.2
